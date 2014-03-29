0aab6471e8d0a0d5b46cf838cda4caf08269c6b0
Peter Barnes <barnespeter213@gmail.com>
2014-03-29 13:00:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RepairsApp::Application.config.secret_key_base = 'f6331d7bd04464667a48558accab9cedca61ce17eb2e7728cd4773ac757b7ed87d59a486333e82d360bdc8cad61c9cf24006a9dd0512477b06add23ec9e9ca17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RepairsApp::Application.config.session_store :cookie_store, key: '_repairs_app_session'
