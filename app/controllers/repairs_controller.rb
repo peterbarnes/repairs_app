eaacbd90a50acea724a42b6610f515adc8ad94a5
Peter Barnes <barnespeter213@gmail.com>
2014-04-01 14:23:32 -0500

    Create repair form

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -4,6 +4,21 @@ class RepairsController < ApplicationController
 	end
 
 	def new
-		
+		@repair = Repair.new
 	end
+
+	def create
+		@repair = Repair.new(repair_params)
+		if @repair.save
+			redirect_to @repair
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+		def repair_params
+			params.require(:repair).permit(:customer, :item, :symptoms)
+		end
 end
